7ca0ce294798d540b3d1dff18e0108674c4b6f06
Yashraj Agarwal <50520646+yashraj236@users.noreply.github.com>
2025-01-12 11:05:06 +0530

    Add files via upload

---
+++ rest-demo/src/main/java/com/thinkconstructive/rest_demo/model/CloudVendor.java
@@ -0,0 +1,49 @@
+package com.thinkconstructive.rest_demo.model;
+
+public class CloudVendor {
+    private String vendorId;
+    private String vendorName;
+    private String vendorAddress;
+    private String vendorPhoneNumber;
+    public CloudVendor(){
+
+    }
+    public CloudVendor(String vendorId, String vendorName, String vendorAddress, String vendorPhoneNumber) {
+        this.vendorId = vendorId;
+        this.vendorName = vendorName;
+        this.vendorAddress = vendorAddress;
+        this.vendorPhoneNumber = vendorPhoneNumber;
+    }
+
+    public void setVendorId(String vendorId) {
+        this.vendorId = vendorId;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+
+    public void setVendorAddress(String vendorAddress) {
+        this.vendorAddress = vendorAddress;
+    }
+
+    public void setVendorPhoneNumber(String vendorPhoneNumber) {
+        this.vendorPhoneNumber = vendorPhoneNumber;
+    }
+
+    public String getVendorId() {
+        return vendorId;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public String getVendorAddress() {
+        return vendorAddress;
+    }
+
+    public String getVendorPhoneNumber() {
+        return vendorPhoneNumber;
+    }
+}
